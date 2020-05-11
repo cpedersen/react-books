f482d874a12304b498f2672069ddc641946d02d0
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-05-11 00:10:20 -0400

    Add logic for printing multiple authors, price if it's available, description if it's available, add separator line between books

--- src/displayBooklist.js
+++ src/displayBooklist.js
@@ -36,21 +36,108 @@ class DisplayBooklist extends Component {
         console.log("books.items: ", this.props.books)
         let data = "No books found";
         
-        ////Print title, author, price, description, image
-        //<p>{item.searchInfo.textSnippet}</p>
-
         //If books found, then display the books
         if (this.props.books.length) {
+            
             //Loop through each book
             data = this.props.books.map((item, i) => {
-                //If book is for sale then display price
+                //Get list of authors
+                const numAuthors = item.volumeInfo.authors.length;
+                let listAuthors = []
+                if (numAuthors == 2) {
+                    for (let i = 1; i <= item.volumeInfo.authors.length; i++) {
+                        let author = item.volumeInfo.authors[i];
+                        listAuthors.push(author)
+                        if (i != 2) {
+                            listAuthors.push(" and ")
+                        }
+                    }
+                } else if (numAuthors > 2) {
+                    for (let i = 0; i <= item.volumeInfo.authors.length; i++) {
+                        let author = item.volumeInfo.authors[i];
+                        listAuthors.push(author)
+                        if (i < item.volumeInfo.authors.length - 1) {
+                            listAuthors.push(", ")
+                        } 
+                    }
+                } else {
+                    listAuthors = item.volumeInfo.authors;
+                }
+
+                //If book is not for sale, don't display price
                 if (item.saleInfo.saleability === "NOT_FOR_SALE") {
-                    return (
-                        <div key={i}>
-                            <h1>{item.volumeInfo.title}</h1>
-                            <p>{item.volumeInfo.authors}</p>
-                        </div>
-                    );
+                    let foundDescErr = 0;
+                    try {
+                        let description = item.volumeInfo.description;
+                    }
+                    catch(err) {
+                        console.log("Couldn't find description: ", err);
+                        foundDescErr = 1;
+                    }
+                    if (foundDescErr !== 0) {
+                        return (
+                            <div key={i}>
+                                <h1>{item.volumeInfo.title}</h1>
+                                <h2>{item.volumeInfo.subtitle}</h2>
+                                <div>Author: {listAuthors}</div>
+                                <hr className="divider"></hr>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div key={i}>
+                                <h1>{item.volumeInfo.title}</h1>
+                                <h2>{item.volumeInfo.subtitle}</h2>
+                                <div>Author: {listAuthors}</div>
+                                <p>{item.volumeInfo.description}</p>
+                                <hr className="divider"></hr>
+                            </div>
+                        );   
+                    }
+
+                } else {
+                    //Format price correctly
+                    let price = item.saleInfo.listPrice.amount;
+                    if (Number.isInteger(price)) {
+                        price = "$" + item.saleInfo.listPrice.amount + ".00";
+                    } else {
+                        price = "$" + item.saleInfo.listPrice.amount;
+                    }
+
+                    let foundDescErr = 0;
+                    try {
+                        let description = item.volumeInfo.description;
+                    }
+                    catch(err) {
+                        console.log("Couldn't find description: ", err);
+                        foundDescErr = 1;
+                    }
+                    if (foundDescErr !== 0) {
+                        return (
+                            <div key={i}>
+                                <h1>{item.volumeInfo.title}</h1>
+                                <h2>{item.volumeInfo.subtitle}</h2>
+                                <div>
+                                    <div>Author: {listAuthors}</div>
+                                    <div>Price: {price}</div>
+                                </div>
+                                <hr className="divider"></hr>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div key={i}>
+                                <h1>{item.volumeInfo.title}</h1>
+                                <h2>{item.volumeInfo.subtitle}</h2>
+                                <div>
+                                    <div>Author: {listAuthors}</div>
+                                    <div>Price: {price}</div>
+                                </div>
+                                <p>{item.volumeInfo.description}</p>
+                                <hr className="divider"></hr>
+                            </div>
+                        );
+                    }
                 }
             }); //map
         
